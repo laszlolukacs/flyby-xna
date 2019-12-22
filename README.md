cffa062ba14fb69a131b1d84e359bce4936ab142
Laszlo Lukacs <laci.28@outlook.com>
2019-12-22 20:42:48 +0100

    Migrated to .NET Core 3.1 and MonoGame 3.7
    The game runs fine, however some of the shaders (normal mapping and water reflection) are broken.

--- README.md
+++ README.md
@@ -2,30 +2,25 @@
 The XNA Flyby is an XNA powered flight game, originally created in 2012 as a homework for a [course](https://www.aut.bme.hu/Course/VIAUJV01).
 
 ## Dependencies ##
-* [.NET 4.0 Framework](https://www.microsoft.com/en-us/download/details.aspx?id=17851)
-* [Visual Studio 2010](https://msdn.microsoft.com/en-us/library/dd831853(v=vs.100).aspx) with [SP1](https://www.microsoft.com/en-us/download/details.aspx?id=23691) - newer VS versions can be buffed to open XNA projects, [details here](https://mxa.codeplex.com/releases/view/618279)
-* [XNA Game Studio 4.0 Refresh](https://www.microsoft.com/en-us/download/details.aspx?id=27598)
-* Highly recommended: [NShader](https://nshader.codeplex.com/) extension for VS2010 / [HLSL Tools](https://github.com/tgjones/HlslTools) extension for VS2013/2015
+* [.NET Core SDK 3.1.100](https://dotnet.microsoft.com/download/dotnet-core/3.1)
 
 ## System Requirements ##
 * 1.4 GHz x86 processor
 * 512 MB memory
-* Shader Model 2.0 (DirectX 9) capable video card with 128 MB memory (practically any video chip should work which is released after 2005)
-* Windows XP SP3 or any newer
-* [.NET 4.0 Framework](https://www.microsoft.com/en-us/download/details.aspx?id=17851) and [XNA Framework 4.0 Runtime Refresh](https://www.microsoft.com/en-us/download/details.aspx?id=27598)
+* Shader Model 2.0 / OpenGL 2.0 capable video card with 128 MB memory and working OpenGL driver
+* Sound card
+* [.NET Core Desktop Runtime 3.1.0](https://dotnet.microsoft.com/download/dotnet-core/3.1)
 
 ## Summary of set up
 * `git clone git@github.com:laszlolukacs/flyby-xna.git <LOCAL_WORKING_DIR>`
-* Install the fonts from the `XnaFlyby.Assets/Fonts` directory
-* Open the `XnaFlyby.sln` in Visual Studio 2010
+* Install the fonts from the `src/Flyby.Application/Contents/Fonts` directory
+* `dotnet restore`
 
 ## Basic Usage / How to extend
 TBD
 
 ## Notes
-The game uses the `Reach` *XNA profile* for maximizing compatibility and minimizing system requirements. See the [differences between `Reach` and `Hi-Def` *XNA profiles*](https://blogs.msdn.microsoft.com/shawnhar/2010/03/12/reach-vs-hidef/).
-
-When the game was created I was a happy owner of an IBM ThinkPad T60 machine equipped with the [ATI Mobility Radeon X1400](https://en.wikipedia.org/wiki/List_of_AMD_graphics_processing_units#Mobility_Radeon_X1xxx_Series) GPU. Because of this GPU I was forced to work with the `Reach` *profile* and I quite much stuck with that.
+TBD
 
 ## Deployment
 TBD
---
+++ src/Flyby.Application/Entities/HUD.cs
@@ -0,0 +1,99 @@
+﻿// <copyright file="Hud.cs" company=".">
+// See license.md for details.
+// </copyright>
+
+namespace XnaFlyby.Game.Entities
+{
+    using System.Collections.Generic;
+    using Microsoft.Xna.Framework;
+    using Microsoft.Xna.Framework.Graphics;
+
+    /// <summary>
+    /// Represents the Heads-up Display.
+    /// </summary>
+    public class Hud
+    {
+        /// <summary>
+        /// Gets or sets the sprite batch.
+        /// </summary>
+        protected SpriteBatch SpriteBatch { get; set; }
+
+        /// <summary>
+        /// Gets or sets the collection of the HUD's sprite fonts.
+        /// </summary>
+        public IList<SpriteFont> SpriteFonts { get; set; }
+
+#if DEBUG
+        public string AdapterName { get; set; }
+        public int FramesPerSecond { get; set; }
+#endif
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public float Altitude { get; set; }
+        public float TerrainHeight { get; set; }
+        public float Velocity { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the overlay help is displayed.
+        /// </summary>
+        public bool IsHelpEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the help overlay texture.
+        /// </summary>
+        public Texture2D HelpOverlay { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Hud"/> class.
+        /// </summary>
+        /// <param name="spriteBatch">The sprite batch.</param>
+        /// <param name="width">The width of the screen.</param>
+        /// <param name="height">The height of the screen.</param>
+        public Hud(SpriteBatch spriteBatch, int width, int height)
+        {
+            this.SpriteBatch = spriteBatch;
+            this.SpriteFonts = new List<SpriteFont>();
+
+            this.Width = width;
+            this.Height = height;
+
+            this.IsHelpEnabled = false;
+
+            //this.HelpOverlay = helpOverlay; // TEXTURE2D
+        }
+
+        /// <summary>
+        /// Draws the heads-up display.
+        /// </summary>
+        /// <param name="gameTime">The game time.</param>
+        public void Draw(GameTime gameTime)
+        {
+            this.SpriteBatch.Begin();
+
+            this.SpriteBatch.DrawString(this.SpriteFonts[0], "Altitude", new Vector2(this.Width - 120, this.Height - 140), Color.LightGreen);
+            this.SpriteBatch.DrawString(this.SpriteFonts[1], this.Altitude.ToString("00.00"), new Vector2(this.Width - 200, this.Height - 110), Color.LightGreen);
+
+#if DEBUG
+            this.SpriteBatch.DrawString(this.SpriteFonts[0], "Height", new Vector2(this.Width - 120, this.Height - 70), Color.LightGreen);
+            this.SpriteBatch.DrawString(this.SpriteFonts[1], this.TerrainHeight.ToString("00.00"), new Vector2(this.Width - 200, this.Height - 40), Color.LightGreen);
+#else
+            SpriteBatch.DrawString(SpriteFonts[0], "Velocity", new Vector2(Width - 120, Height - 70), Color.LightGreen);
+            SpriteBatch.DrawString(SpriteFonts[1], Velocity.ToString("00.00"), new Vector2(Width - 200, Height - 40), Color.LightGreen);
+#endif
+
+            // TODO: fix this
+            //if (this.IsHelpEnabled)
+            //{
+            //    this.SpriteBatch.Draw(this.HelpOverlay, new Rectangle(0, (this.Height - this.Width) / 2, this.Width, this.Width), Color.White);
+            //}
+
+#if DEBUG
+            this.SpriteBatch.DrawString(this.SpriteFonts[2], string.Format("{00} FPS", this.FramesPerSecond), new Vector2(0, 0), Color.Red);
+            this.SpriteBatch.DrawString(this.SpriteFonts[2], string.Format("Rendering on {0}", this.AdapterName), new Vector2(0, 20), Color.Red);
+#endif
+
+            this.SpriteBatch.End();
+        }
+    }
+}
